edb4c8254059ad13684de6ee336746a7a73fa663
LumoFace <lumoface@proton.me>
2022-05-27 21:26:28 +0000

    approved output

--- VOClabelcolormap.py
+++ VOClabelcolormap.py
@@ -20,3 +20,27 @@ def color_map(N=256, normalized=False):
         for j in range(8):
             r = r | (bitget(c, 0) << 7-j)
             g = g | (bitget(c, 1) << 7-j)
+            b = b | (bitget(c, 2) << 7-j)
+            c = c >> 3
+
+        cmap[i] = np.array([r, g, b])
+
+    cmap = cmap/255 if normalized else cmap
+    return cmap
+
+
+def color_map_viz():
+    labels = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor', 'void']
+    nclasses = 21
+    row_size = 50
+    col_size = 500
+    cmap = color_map()
+    array = np.empty((row_size*(nclasses+1), col_size, cmap.shape[1]), dtype=cmap.dtype)
+    for i in range(nclasses):
+        array[i*row_size:i*row_size+row_size, :] = cmap[i]
+    array[nclasses*row_size:nclasses*row_size+row_size, :] = cmap[-1]
+
+    imshow(array)
+    plt.yticks([row_size*i+row_size/2 for i in range(nclasses+1)], labels)
+    plt.xticks([])
+    plt.show()
