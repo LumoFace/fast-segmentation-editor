1748420447762f478298797a8a196462acc0dd44
LumoFace <lumoface@proton.me>
2022-07-10 09:32:18 +0000

    Update cmakeify-production

--- truth_and_crop_qt4.h
+++ truth_and_crop_qt4.h
@@ -37,3 +37,75 @@ class Ui_TruthAndCrop
 {
 public:
     QWidget *centralwidget;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents_2;
+    QHBoxLayout *horizontalLayout;
+    QLabel *img_view;
+    QWidget *horizontalLayoutWidget;
+    QVBoxLayout *verticalLayout_4;
+    QSpinBox *compactnessBox;
+    QSpinBox *wndBox;
+    QSpinBox *segmentsBox;
+    QSpinBox *sigmaBox;
+    QSpinBox *dsBox;
+    QWidget *horizontalLayoutWidget_2;
+    QVBoxLayout *verticalLayout_5;
+    QLabel *label_3;
+    QLabel *label_8;
+    QLabel *label_2;
+    QLabel *label;
+    QLabel *label_7;
+    QWidget *horizontalLayoutWidget_3;
+    QHBoxLayout *horizontalLayout_4;
+    QVBoxLayout *verticalLayout;
+    QRadioButton *class_other;
+    QRadioButton *class_mussel;
+    QRadioButton *class_ciona;
+    QRadioButton *class_styela;
+    QVBoxLayout *verticalLayout_2;
+    QPushButton *refreshBtn;
+    QPushButton *cropBtn;
+    QPushButton *toggleBtn;
+    QPushButton *doneBtn;
+    QCheckBox *enforceConnectivityBox;
+    QWidget *verticalLayoutWidget_3;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label_6;
+    QTextEdit *imageField;
+    QLabel *label_4;
+    QTextEdit *outputPath;
+    QProgressBar *progressBar;
+    QWidget *horizontalLayoutWidget_4;
+    QHBoxLayout *horizontalLayout_5;
+    QPushButton *inFile;
+    QPushButton *outFile;
+    QWidget *horizontalLayoutWidget_5;
+    QHBoxLayout *horizontalLayout_6;
+    QPushButton *previousBtn;
+    QPushButton *outFile_2;
+    QLabel *label_5;
+    QGroupBox *groupBox;
+    QGroupBox *groupBox_2;
+    QGroupBox *groupBox_3;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *TruthAndCrop)
+    {
+        if (TruthAndCrop->objectName().isEmpty())
+            TruthAndCrop->setObjectName(QStringLiteral("TruthAndCrop"));
+        TruthAndCrop->resize(1209, 923);
+        centralwidget = new QWidget(TruthAndCrop);
+        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        scrollArea = new QScrollArea(centralwidget);
+        scrollArea->setObjectName(QStringLiteral("scrollArea"));
+        scrollArea->setGeometry(QRect(10, 10, 871, 801));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents_2 = new QWidget();
+        scrollAreaWidgetContents_2->setObjectName(QStringLiteral("scrollAreaWidgetContents_2"));
+        scrollAreaWidgetContents_2->setGeometry(QRect(0, 0, 869, 799));
+        horizontalLayout = new QHBoxLayout(scrollAreaWidgetContents_2);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        img_view = new QLabel(scrollAreaWidgetContents_2);
+        img_view->setObjectName(QStringLiteral("img_view"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
