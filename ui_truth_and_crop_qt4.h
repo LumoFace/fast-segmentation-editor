9f83a5e15743b4bba5179a53116c6567c809514c
LumoFace <lumoface@proton.me>
2022-08-24 19:50:09 +0000

    Updated README API Docs

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -1,4 +1,6 @@
 /********************************************************************************
 ** Form generated from reading UI file 'truth_and_crop_qt4Sa2743.ui'
 **
-** Created by: Qt User Interface Compil
+** Created by: Qt User Interface Compiler version 5.6.0
+**
+** WARNING! All changes made in this file will be 
