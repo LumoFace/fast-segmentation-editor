cfd59795145efeedbf18bc8f202918264beed0a0
LumoFace <lumoface@proton.me>
2022-08-24 18:31:51 +0000

    clipboard

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -1,2 +1,4 @@
 /********************************************************************************
-** Form generated from reading U
+** Form generated from reading UI file 'truth_and_crop_qt4Sa2743.ui'
+**
+** Created by: Qt User Interface Compil
