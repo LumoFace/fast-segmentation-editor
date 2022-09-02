3a88f15c169daae0ae67cfc680a1ee9e372fe746
LumoFace <lumoface@proton.me>
2022-09-02 12:14:46 +0000

    workflow

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -3,4 +3,5 @@
 **
 ** Created by: Qt User Interface Compiler version 5.6.0
 **
-** WARNING! All changes made in this file will be 
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************
