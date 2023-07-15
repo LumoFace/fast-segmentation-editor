293940987c44d0811798401cc8fb4666bcc7c300
LumoFace <lumoface@proton.me>
2023-07-15 13:08:01 +0000

    Add missing license headers

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -262,4 +262,5 @@ public:
 
         verticalLayout_6->addWidget(lcdNumber_3);
 
-        lcdNumber_4 = new
+        lcdNumber_4 = new QLCDNumber(horizontalLayoutWidget_3);
+        lcdNumber_4->setObjectName(QS
