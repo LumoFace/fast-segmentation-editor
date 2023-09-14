8089c91b07356ffbe9603e6d10ff3f7516d609b9
LumoFace <lumoface@proton.me>
2023-09-14 14:28:28 +0000

    update travis build

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -381,4 +381,5 @@ public:
         groupBox_2->setObjectName(QStringLiteral("groupBox_2"));
         groupBox_2->setGeometry(QRect(890, -10, 311, 351));
         groupBox_3 = new QGroupBox(centralwidget);
-        groupBox_3->setObjectName(QStringLit
+        groupBox_3->setObjectName(QStringLiteral("groupBox_3"));
+        groupBox_3->setGeometry(QRect(890, 
