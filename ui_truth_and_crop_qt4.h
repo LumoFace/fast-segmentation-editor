ccf7f2a475fb2d68f749a92a2a61530cda22b19d
LumoFace <lumoface@proton.me>
2023-10-29 11:16:27 +0000

    base.html

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -440,4 +440,4 @@ public:
         imageField->setHtml(QApplication::translate("TruthAndCrop", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></h
+"</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-s
