65ad9ef092094077ebbbcf35260e0529e26cdcf9
LumoFace <lumoface@proton.me>
2023-11-07 11:23:15 +0000

    Clean ups.

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -447,4 +447,4 @@ public:
         outputPath->setHtml(QApplication::translate("TruthAndCrop", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head>
+"</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-
