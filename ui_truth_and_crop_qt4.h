c5634d1b0b0404519b8d25f0cfbbd5b6df1a2520
LumoFace <lumoface@proton.me>
2023-11-12 22:28:32 +0000

    Upgrade JSorolla dependency

--- ui_truth_and_crop_qt4.h
+++ ui_truth_and_crop_qt4.h
@@ -450,4 +450,5 @@ public:
 "</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Ubuntu'; font-size:11pt;\"><br /></p></body></html>", 0));
         inFile->setText(QApplication::translate("TruthAndCrop", "Input File", 0));
-        outFile->setText(QApplication::tra
+        outFile->setText(QApplication::translate("TruthAndCrop", "Output Path", 0));
+        previousBtn->setText(QAp
